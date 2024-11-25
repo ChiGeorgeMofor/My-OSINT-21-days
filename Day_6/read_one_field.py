720173c7f3c2686c21dbdd3dd51fd271b7ee4875
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-11-25 23:58:55 +0100

    Update read_one_field.py

--- Day_6/read_one_field.py
+++ Day_6/read_one_field.py
@@ -3,7 +3,7 @@ import requests
 
 response = requests.get("https://api.github.com/search/users?q=forkim")
 # q in the users?q=... is the query parameter
-
+ 
 json_data = response.json()
 
 print(json_data['total_count'])
