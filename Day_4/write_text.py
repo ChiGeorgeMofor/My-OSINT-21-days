4c581c110d00da4aadd508327db3bfa2ac234384
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-12-11 08:43:25 +0100

    Update write_text.py

--- Day_4/write_text.py
+++ Day_4/write_text.py
@@ -2,7 +2,7 @@ result = "Results text"
 
 results_file = open("results.txt", "a")
 # The open() function has two arguments, the first the name of the file and the second the "opening mode"
-# "r" - open a file for reading
+# "r" - open a file for reading 
 # "a" - open a file for appending 
 # "w" - open a file for writing 
 # "x" - create new file 
