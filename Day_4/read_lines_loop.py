b440dd6b7993a67a2b8ad67090f7851ba8a5368e
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-12-12 00:16:44 +0100

    Update read_lines_loop.py

--- Day_4/read_lines_loop.py
+++ Day_4/read_lines_loop.py
@@ -3,7 +3,7 @@ stringNumber = 1
 
 with open("results.txt") as f:
     for line in f: 
-        print(str(stringNumber) + ". " + line)
+        print(str(stringNumber) + ". " + line) 
         stringNumber += 1  
 
 # We used the str() to convert a variable of type integer to a string.
