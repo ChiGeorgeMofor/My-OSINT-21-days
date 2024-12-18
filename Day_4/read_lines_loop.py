bca5209a675ed00d884bbd65a53c9b139da39fc5
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2024-12-18 11:36:33 +0300

    Update read_lines_loop.py

--- Day_4/read_lines_loop.py
+++ Day_4/read_lines_loop.py
@@ -1,7 +1,7 @@
 
 stringNumber = 1
 
-with open("results.txt") as f:
+with open("results.txt") as f: 
     for line in f: 
         print(str(stringNumber) + ". " + line) 
         stringNumber += 1  
