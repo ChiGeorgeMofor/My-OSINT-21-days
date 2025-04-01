5f303bd9d1bd16c043e954dadf9c552a11db733c
ChiGeorgeMofor <105949693+ChiGeorgeMofor@users.noreply.github.com>
2025-03-31 21:43:09 -1100

    Update webapp.py

--- Day_20/webapp.py
+++ Day_20/webapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('New Web Application')
 
-textInput = st.text_input('Enter your name:', "Chi George Mofor")
+textInput = st.text_input('Enter your name:', "Chi George Mofor") 
 
 if(st.button('Submit')): 
     nickname = textInput.title()
